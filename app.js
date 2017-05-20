c75ba290b53f6fc973c2788c53740f65735d0671
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2017-05-20 20:45:11 +0530

    Changed to correct port number

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.post('/video', (req, res) => {
     });
 });
 
-app.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8081, function(){
-    console.log("Server started on port ", process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8081);
+app.listen(process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080, function(){
+    console.log("Server started on port ", process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 });
